e1f61412945d454fcf281aebc2bdffb88c02efc6
kurorodev <danyakud13@gmail.com>
2023-12-14 22:12:10 +0400

    sdlfkaf

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from dataset import temprature as t
 from dataset import home_specific as hs
 from dataset import temprature_size as ts
+from model import model 
 
 def main():
     hs.homeSpecific()
+    print("<-------------------------------------------------------->")
+    model.compile_and_fit()
 
 
 if __name__ == "__main__":
